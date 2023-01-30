25508b5de7b4b07c8e6a6a848db5e4fce62ff51f
Matheus Honorio <matheushonorio@eng.ci.ufpb.br>
2023-01-30 10:39:39 -0300

    first class finished

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get("/", async () => {
+  const habits = prisma.habit.findMany();
+
+  return habits;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server is running on port 3333");
+  });
